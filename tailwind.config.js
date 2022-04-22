5877ebe96f14239eda8261b3f4b91940e2d6b07b
Rendi Dwi Francisko <90760961+Rendyfranzz@users.noreply.github.com>
2022-04-22 13:45:50 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': 'url("./image/menu.jpg")',
+        'hutan':'url("./image/hutan.jpg")',
+        'petir': 'url("./image/petir.png")',
+        'hujan':'url("./image/rain.png")'
+      },
+      fontFamily: {
+        'zila' : ['Zilla Slab'],
+      },
+    },
   },
   plugins: [],
 }
