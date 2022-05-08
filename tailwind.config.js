782597a3bd7341dd72f17a8690d57a8b20061a35
Rendi Dwi Francisko <90760961+Rendyfranzz@users.noreply.github.com>
2022-05-08 13:30:28 +0700

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-image': 'url("./image/menu.jpg")',
+        'hutan':'url("./image/hutan.jpg")',
+        'petir': 'url("./image/petir.png")',
+        'hujan':'url("./image/rain.png")'
+      },
+      fontFamily: {
+        'zila' : ['Zilla Slab'],
+        'creeper':['Creepster'],
+      },
+      colors: {
+        'regal': '#2be4e4',
+      },
+    },
+  },
+  plugins: [],
+}
